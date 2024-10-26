7bc263f0f9eed34a3fed08f76e13288346c8c172
Mark Orcullo <orcullomark856@gmail.com>
2024-10-26 21:28:11 +0800

    POS nav complete

--- SmartPoultry/home.xaml.cs
+++ SmartPoultry/home.xaml.cs
@@ -21,6 +21,12 @@ namespace SmartPoultry
     /// </summary>
     public partial class home : UserControl
     {
+
+        public static String[] buttonAnimalArray = { "animalAllBtn", "animalChickenBtn", "animalDogBtn", "animalCatBtn", "animalPigBtn", "animalDuckBtn", "animalCowBtn", "animalHorseBtn", "animalRabbitBtn", "animalBirdBtn", "animalFishBtn", "animalGuineaBtn" };
+        public static String[] borderAnimalArray = { "animalAllBorder", "animalChickenBorder", "animalDogBorder", "animalCatBorder", "animalPigBorder", "animalDuckBorder", "animalCowBorder", "animalHorseBorder", "animalRabbitBorder", "animalBirdBorder", "animalFishBorder", "animalGuineaBorder" };
+
+        public static String[] buttonTypeArray = { "typeAllBtn", "typeFeedsBtn", "typeMedicineBtn", "typeVitaminsBtn", "typeAccessoriesBtn", "typeVaccinesBtn"};
+        public static String[] borderTypeArray = { "typeAllBorder", "typeFeedsBorder", "typeMedicineBorder", "typeVitaminsBorder", "typeAccessoriesBorder", "typeVaccinesBorder" };
         public home()
         {
             InitializeComponent();
@@ -32,81 +38,325 @@ namespace SmartPoultry
 
         }
 
-        //POS Buttons Click Functions
+        //POS Buttons Click Functions (Animal type)
         private void AllButton_Click(object sender, RoutedEventArgs e)
         {
-            ActiveButton(animalAllBtn, animalAllBorder);
-            InactiveButton(animalChickenBtn, animalChickenBorder);
-            InactiveButton(animalDogBtn, animalDogBorder);
-            InactiveButton(animalCatBtn, animalCatBorder);
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalAllBtn");
+            borderAnimalList.Remove("animalAllBorder");
+
+            ActiveButton("animalAllBtn", "animalAllBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
         }
+
         private void ChickenButton_Click(object sender, RoutedEventArgs e)
         {
-            InactiveButton(animalAllBtn, animalAllBorder);
-            ActiveButton(animalChickenBtn, animalChickenBorder);
-            InactiveButton(animalDogBtn, animalDogBorder);
-            InactiveButton(animalCatBtn, animalCatBorder);
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalChickenBtn");
+            borderAnimalList.Remove("animalChickenBorder");
+
+            ActiveButton("animalChickenBtn", "animalChickenBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
         }
         private void DogButton_Click(object sender, RoutedEventArgs e)
         {
-            InactiveButton(animalAllBtn, animalAllBorder);
-            InactiveButton(animalChickenBtn, animalChickenBorder);
-            ActiveButton(animalDogBtn, animalDogBorder);
-            InactiveButton(animalCatBtn, animalCatBorder);
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalDogBtn");
+            borderAnimalList.Remove("animalDogBorder");
+
+            ActiveButton("animalDogBtn", "animalDogBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
         }
         private void CatButton_Click(object sender, RoutedEventArgs e)
         {
-            InactiveButton(animalAllBtn, animalAllBorder);
-            InactiveButton(animalChickenBtn, animalChickenBorder);
-            InactiveButton(animalDogBtn, animalDogBorder);
-            ActiveButton(animalCatBtn, animalCatBorder);
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalCatBtn");
+            borderAnimalList.Remove("animalCatBorder");
+
+            ActiveButton("animalCatBtn", "animalCatBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
         }
+        private void PigButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalPigBtn");
+            borderAnimalList.Remove("animalPigBorder");
+
+            ActiveButton("animalPigBtn", "animalPigBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+        private void DuckButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalDuckBtn");
+            borderAnimalList.Remove("animalDuckBorder");
+
+            ActiveButton("animalDuckBtn", "animalDuckBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+        private void CowButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalCowBtn");
+            borderAnimalList.Remove("animalCowBorder");
+
+            ActiveButton("animalCowBtn", "animalCowBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+        private void HorseButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalHorseBtn");
+            borderAnimalList.Remove("animalHorseBorder");
+
+            ActiveButton("animalHorseBtn", "animalHorseBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+        private void RabbitButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalRabbitBtn");
+            borderAnimalList.Remove("animalRabbitBorder");
+
+            ActiveButton("animalRabbitBtn", "animalRabbitBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+        private void BirdButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalBirdBtn");
+            borderAnimalList.Remove("animalBirdBorder");
+
+            ActiveButton("animalBirdBtn", "animalBirdBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+        private void FishButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalFishBtn");
+            borderAnimalList.Remove("animalFishBorder");
+
+            ActiveButton("animalFishBtn", "animalFishBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+        private void GuineaButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonAnimalList = new List<String>(buttonAnimalArray);
+            List<String> borderAnimalList = new List<String>(borderAnimalArray);
+
+            buttonAnimalList.Remove("animalGuineaBtn");
+            borderAnimalList.Remove("animalGuineaBorder");
+
+            ActiveButton("animalGuineaBtn", "animalGuineaBorder");
+
+            for (int i = 0; i < buttonAnimalList.Count; i++)
+            {
+                InactiveButton(buttonAnimalList[i], borderAnimalList[i]);
+            }
+        }
+
+
+        //POS Buttons Click Functions (Product type)
         private void TypeAllButton_Click(object sender, RoutedEventArgs e)
         {
-            ActiveButton(typeAllBtn, typeAllBorder);
-            InactiveButton(typeFoodBtn, typeFoodBorder);
-            InactiveButton(typeToysBtn, typeToysBorder);
-            InactiveButton(typeMedsBtn, typeMedsBorder);
+            List<String> buttonTypeList = new List<String>(buttonTypeArray);
+            List<String> borderTypeList = new List<String>(borderTypeArray);
+
+            buttonTypeList.Remove("typeAllBtn");
+            borderTypeList.Remove("typeAllBorder");
+
+            ActiveButton("typeAllBtn", "typeAllBorder");
+
+            for (int i = 0; i < buttonTypeList.Count; i++)
+            {
+                InactiveButton(buttonTypeList[i], borderTypeList[i]);
+            }
         }
-        private void FoodButton_Click(object sender, RoutedEventArgs e)
+        private void FeedsButton_Click(object sender, RoutedEventArgs e)
         {
-            InactiveButton(typeAllBtn, typeAllBorder);
-            ActiveButton(typeFoodBtn, typeFoodBorder);
-            InactiveButton(typeToysBtn, typeToysBorder);
-            InactiveButton(typeMedsBtn, typeMedsBorder);
+            List<String> buttonTypeList = new List<String>(buttonTypeArray);
+            List<String> borderTypeList = new List<String>(borderTypeArray);
+
+            buttonTypeList.Remove("typeFeedsBtn");
+            borderTypeList.Remove("typeFeedsBorder");
+
+            ActiveButton("typeFeedsBtn", "typeFeedsBorder");
+
+            for (int i = 0; i < buttonTypeList.Count; i++)
+            {
+                InactiveButton(buttonTypeList[i], borderTypeList[i]);
+            }
         }
-        private void ToysButton_Click(object sender, RoutedEventArgs e)
+        private void MedicineButton_Click(object sender, RoutedEventArgs e)
         {
-            InactiveButton(typeAllBtn, typeAllBorder);
-            InactiveButton(typeFoodBtn, typeFoodBorder);
-            ActiveButton(typeToysBtn, typeToysBorder);
-            InactiveButton(typeMedsBtn, typeMedsBorder);
+            List<String> buttonTypeList = new List<String>(buttonTypeArray);
+            List<String> borderTypeList = new List<String>(borderTypeArray);
+
+            buttonTypeList.Remove("typeMedicineBtn");
+            borderTypeList.Remove("typeMedicineBorder");
+
+            ActiveButton("typeMedicineBtn", "typeMedicineBorder");
+
+            for (int i = 0; i < buttonTypeList.Count; i++)
+            {
+                InactiveButton(buttonTypeList[i], borderTypeList[i]);
+            }
         }
-        private void MedsButton_Click(object sender, RoutedEventArgs e)
+        private void VitaminsButton_Click(object sender, RoutedEventArgs e)
         {
-            InactiveButton(typeAllBtn, typeAllBorder);
-            InactiveButton(typeFoodBtn, typeFoodBorder);
-            InactiveButton(typeToysBtn, typeToysBorder);
-            ActiveButton(typeMedsBtn, typeMedsBorder);
+            List<String> buttonTypeList = new List<String>(buttonTypeArray);
+            List<String> borderTypeList = new List<String>(borderTypeArray);
+
+            buttonTypeList.Remove("typeVitaminsBtn");
+            borderTypeList.Remove("typeVitaminsBorder");
+
+            ActiveButton("typeVitaminsBtn", "typeVitaminsBorder");
+
+            for (int i = 0; i < buttonTypeList.Count; i++)
+            {
+                InactiveButton(buttonTypeList[i], borderTypeList[i]);
+            }
+        }
+        private void AccessoriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonTypeList = new List<String>(buttonTypeArray);
+            List<String> borderTypeList = new List<String>(borderTypeArray);
+
+            buttonTypeList.Remove("typeAccessoriesBtn");
+            borderTypeList.Remove("typeAccessoriesBorder");
+
+            ActiveButton("typeAccessoriesBtn", "typeAccessoriesBorder");
+
+            for (int i = 0; i < buttonTypeList.Count; i++)
+            {
+                InactiveButton(buttonTypeList[i], borderTypeList[i]);
+            }
+        }
+        private void VaccinesButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> buttonTypeList = new List<String>(buttonTypeArray);
+            List<String> borderTypeList = new List<String>(borderTypeArray);
+
+            buttonTypeList.Remove("typeVaccinesBtn");
+            borderTypeList.Remove("typeVaccinesBorder");
+
+            ActiveButton("typeVaccinesBtn", "typeVaccinesBorder");
+
+            for (int i = 0; i < buttonTypeList.Count; i++)
+            {
+                InactiveButton(buttonTypeList[i], borderTypeList[i]);
+            }
         }
 
 
         //Active and inactive button properties change
-        private void ActiveButton(Button button, Border border)
+        private void ActiveButton(String stringbutton, String stringborder)
         {
-            border.Background = new SolidColorBrush(Color.FromRgb(192, 228, 190));
-            border.BorderBrush = new SolidColorBrush(Color.FromRgb(102, 194, 101));
-            button.Background = new SolidColorBrush(Color.FromRgb(192, 228, 190));
-            button.BorderBrush = new SolidColorBrush(Color.FromRgb(192, 228, 190));
-            button.Foreground = new SolidColorBrush(Color.FromRgb(102, 194, 101));
+            Button button = FindName(stringbutton) as Button;
+            Border border = FindName(stringborder) as Border;
+
+            if (button != null && border != null)
+            {
+                // Set the styles for the Border and Button
+                border.Background = new SolidColorBrush(Color.FromRgb(192, 228, 190));
+                border.BorderBrush = new SolidColorBrush(Color.FromRgb(102, 194, 101));
+
+                button.Background = new SolidColorBrush(Color.FromRgb(192, 228, 190));
+                button.BorderBrush = new SolidColorBrush(Color.FromRgb(192, 228, 190));
+                button.Foreground = new SolidColorBrush(Color.FromRgb(102, 194, 101));
+            }
+            else
+            {
+                MessageBox.Show("Button or Border not found.");
+            }
         }
-        private void InactiveButton(Button button, Border border)
+
+        private void InactiveButton(String stringbutton, String stringborder)
         {
-            border.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-            border.BorderBrush = new SolidColorBrush(Color.FromRgb(243, 243, 243));
-            button.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-            button.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-            button.Foreground = new SolidColorBrush(Color.FromRgb(185, 185, 185));
+            Button button = FindName(stringbutton) as Button;
+            Border border = FindName(stringborder) as Border;
+
+
+            if (button != null && border != null)
+            {
+                border.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                border.BorderBrush = new SolidColorBrush(Color.FromRgb(243, 243, 243));
+                button.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                button.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                button.Foreground = new SolidColorBrush(Color.FromRgb(185, 185, 185));
+            }
+            else
+            {
+                MessageBox.Show("Button or Border not found.");
+            }
+
+            
         }
     }
 }
