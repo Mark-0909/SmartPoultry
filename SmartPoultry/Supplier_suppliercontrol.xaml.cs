20fd21da2dfbfbd9b25101f029176aae5d3c6ba3
Gabriel Asturias <gabrielasturias@outlook.com>
2024-11-21 07:58:26 +0800

    amogus cock

---
+++ SmartPoultry/Supplier_suppliercontrol.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartPoultry
+{
+    /// <summary>
+    /// Interaction logic for Supplier_suppliercontrol.xaml
+    /// </summary>
+    public partial class Supplier_suppliercontrol : UserControl
+    {
+        public Supplier_suppliercontrol()
+        {
+            InitializeComponent();
+        }
+    }
+}
