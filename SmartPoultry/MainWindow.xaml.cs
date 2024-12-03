30ac84010ca1b1d85535d2b545294aca5df117a0
Mark Orcullo <orcullomark856@gmail.com>
2024-12-03 11:02:07 +0800

    1

--- SmartPoultry/MainWindow.xaml.cs
+++ SmartPoultry/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace SmartPoultry
             else
             {
                 MainWindowOverlay.Visibility = Visibility.Collapsed;
-                Panel.SetZIndex(MainWindowOverlay, 0);
+                Panel.SetZIndex(MainWindowOverlay, 1);
             }
         }
 
