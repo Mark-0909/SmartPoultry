8819c920024ea5fb33a924de99074744b1f6779d
Mark Orcullo <orcullomark856@gmail.com>
2025-01-05 13:41:52 +0800

    revert

---
+++ SmartPoultry/Supplier_suppliercontrol.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace SmartPoultry
+{
+    public partial class Supplier_Suppliercontrol : UserControl
+    {
+        public Supplier_Suppliercontrol()
+        {
+            InitializeComponent();
+        }
+
+        private void EditButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Handle button click event
+        }
+    }
+}
